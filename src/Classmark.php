05b9a5838c5d9d1b3c752940e213ac3412038ddf
Allen Tullett <a.tullett@kent.ac.uk>
2025-04-08 16:09:48 +0100

    Rewritten parse function
    This was following discussions with Stewart and Simone about what is a valid Classmark.

--- src/Classmark.php
+++ src/Classmark.php
@@ -56,46 +56,78 @@ class Classmark
      */
     public static function parse($classmark)
     {
-
-        // Validate the classmark.
-        if (!is_string($classmark) || !preg_match('/^([a-z\ ]*[A-Z]{1,2}[A-Za-z0-9\.\ ]*)$/', $classmark)) {
-            throw new \InvalidArgumentException('Invalid classmark provided for parse.');
+    
+        // Validate the classmark
+        if($classmark == null || $classmark == '' || empty($classmark)) {
+            throw new \InvalidArgumentException('Invalid classmark provided for parse - No value provided;');
         }
-
+    
         // Setup our variables.
-        $subject = '';
-        $sub_subject = '';
+        $subject = null;
+        $sub_subject = null;
     
-        // Check the first character and if it is a lowercase 'f' or 'q' then remove it
-        if (strlen($classmark) > 1 && preg_match('/(^f|^q)/', $classmark)) {
-            $classmark = substr($classmark, 1);
+        $classmark = strtoupper($classmark); // Uppercase our $classmark string
+    
+        // Does the classmark start with a number?
+        if (is_numeric(substr($classmark, 0, 1))) {
+            throw new \InvalidArgumentException('Invalid classmark provided for parse - Value beings with a number (' . $classmark . ');');
         }
     
-        // Uppercase our $classmark string
-        $classmark = strtoupper($classmark);
+        // PREFIX
+        // ----------------------------------------------------------------------------------
     
-        // Check first 2 characters and if they are 'FF' or 'QQ' then remove the first character
-        // This relates to when there is a 'f' (for folio) or 'q' (for quarto) at the start of the classmark. We will never have a classmark that has 2 'f's or 2 'q's at the start.
-        if (preg_match('/^FF/', $classmark) || preg_match('/^QQ/', $classmark)) {
-            $classmark = substr($classmark, 1);
-        }
-        
-        // Check if the string has 1 or 2 alpha characters at the start and then store as $subject
-        if (preg_match('/^[A-Z][A-Z]/', $classmark)) {
-            $subject = substr($classmark, 0, 2);
-        } else {
-            $subject = substr($classmark, 0, 1);
+        // If the string start with 'FOL', 'LRG', or 'PER'
+        if(substr($classmark, 0, 3) == 'FOL' || substr($classmark, 0, 3) == 'LRG' || substr($classmark, 0, 3) == 'PER') {
+    
+            // If yes - Remove the prefix
+            $classmark = substr($classmark, 3); // Remove the prefix
+            
+            // Check if first character is a space
+            if(substr($classmark, 0, 1) == ' ') {
+                $classmark = substr($classmark, 1); // Remove the space
+            }
+    
         }
     
-        // Trim these characters from the $classmark string and store as $sub_subject
+        // If there is single prefix letter ('F', 'L', 'P', or 'Q') followed by a space / decimal and another letter (If followed by a number then its is valid)
+        $prefixes = ['F', 'L', 'P', 'Q'];
+        if (in_array(substr($classmark, 0, 1), $prefixes) && in_array($classmark[1], [' ', '.'])) {
+            $classmark = substr($classmark, 2); // Remove the character and the space / decimal
+        }
+    
+        // Check if the first two characters are either 'ff', 'll', 'pp', or 'qq'
+        if (preg_match('/^ff|^ll|^pp|^qq/i', $classmark)) {
+            // If yes - Remove the first character and continue
+            $classmark = substr($classmark, 1); // Remove the first character
+        }
+    
+        // How many letters are there at the start?
+        $number_index = 0; // Default value
+        if (preg_match('/\d/', $classmark, $matches, PREG_OFFSET_CAPTURE)) {
+            $number_index = $matches[0][1];
+        }
+    
+        // SUBJECTS
+        // ----------------------------------------------------------------------------------
+    
+        $subject = substr($classmark, 0, $number_index);    
         $sub_subject = trim(substr($classmark, strlen($subject)));
     
-        // If first character is a decimal or space remove it
-        if (preg_match('/(^\.|^\s)/', $sub_subject)) {
-            $sub_subject = trim(substr($sub_subject, 1));
+        // Find and remove any decimal or space on the subject value
+        if (preg_match('/(\.|\s)/', $subject, $matches, PREG_OFFSET_CAPTURE)) {
+            $subject = str_replace([' ','.'], '', $subject);
         }
     
-        // Find index of first space
+        // Check if first letter is a valid classmark
+        // If character is I, O, W, or X = Return false (Invalid classmark)
+        if (preg_match('/^[IOWX]/', $subject)) {
+            throw new \InvalidArgumentException('Invalid classmark provided for parse - First subject letter ' . $subject[0] . ' is invalid (' . $classmark . ');');
+        }
+    
+        // SUB-SUBJECTS
+        // ----------------------------------------------------------------------------------
+    
+        // Search for first single space in the remaining string
         preg_match('/\s/', $sub_subject, $matches, PREG_OFFSET_CAPTURE);
         if (!empty($matches)) {
             // Remove all characters from that index to the end of the string
@@ -103,14 +135,15 @@ class Classmark
             $sub_subject = substr($sub_subject, 0, $index);
         }
     
-        // Check last character for a decimal or space
-        preg_match('/(\.|\s)$/', $sub_subject, $matches, PREG_OFFSET_CAPTURE);
-        if (!empty($matches)) {
-            $index = $matches[0][1];
-            // If it is a space or a decimal, remove it
-            $sub_subject = substr($sub_subject, 0, $index);
+        // Check if first character is a space or decimal
+        if (preg_match('/^(\.|\s)/', $sub_subject)) {
+            $sub_subject = substr($sub_subject, 1); // Remove the character
         }
     
+        if (empty($subject) || empty($sub_subject)) {
+            throw new \InvalidArgumentException('Invalid classmark provided for parse - Parsing returned an empty value for subject and/or sub-subject (' . $classmark . ');');
+        }
+        
         return new static($subject . ' ' . $sub_subject);
 
     }
